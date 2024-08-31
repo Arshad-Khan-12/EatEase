4ee292aaa5d5666b8e699e2072a8b6e97751ff87
Arshad-Khan-12 <ak4566335@gmail.com>
2024-08-31 15:57:54 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // This is the line you need to add to your vite.config.js
+});
